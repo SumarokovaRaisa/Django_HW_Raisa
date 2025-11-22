222b9842102f2c401ba37ad234b68104e1feca50
Sumarokova <sumarokova75@internet.ru>
2025-11-22 15:12:16 +0200

    Clean repo: remove pycache, db; add migrations; update models and admin

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Task, Subtask, Category
+
 
 # Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+    search_fields = ("name",)
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "status", "deadline", "created_at")
+    search_fields = ("title",)
+    filter_horizontal = ("categories",)
+
+@admin.register(Subtask)
+class SubtaskAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "status", "deadline", "created_at")
+    list_filter = ("status", "created_at", "deadline")
+    search_fields = ("title",)
--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    STATUS_CHOICE = [
+        ("new", "New"),
+        ("in_progress", "In_progress"),
+        ("blocked", "Blocked"),
+        ("done", "Done"),
+    ]
+
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    categories = models.ManyToManyField(Category, related_name="tasks", blank=True)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICE, default="new")
+    deadline = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ("title", "created_at")  # уникально для даты
+
+    def __str__(self):
+        return self.title
+
+
+class Subtask(models.Model):
+    STATUS_CHOICE = [
+        ("new", "New"),
+        ("in_progress", "In_progress"),
+        ("blocked", "Blocked"),
+        ("done", "Done"),
+    ]
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name="subtask")
+    status = models.CharField(max_length=20, choices=STATUS_CHOICE, default="new")
+    deadline = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.task.title} -> {self.title}"
